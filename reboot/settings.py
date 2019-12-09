e903eac5c563d27532387b054103bc488550ab6e
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2019-12-09 22:28:20 +0000

    Accounts added as an installed app and authentication backends created to ensure we have login capability

--- reboot/settings.py
+++ reboot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
