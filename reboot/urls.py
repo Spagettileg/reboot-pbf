b6ac23651136c6f95c39707fe230a7a7636fb9f6
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2019-12-10 15:14:16 +0000

    Imported home_view from home.views library and new url pattern created for home_view

--- reboot/urls.py
+++ reboot/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
+from home.views import home_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_view, name="home"),
     url(r'^accounts/', include(urls_accounts)),
 ]
