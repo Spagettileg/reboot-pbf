20d029b93922a030b4b87a8075d0eff537b3814b
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2019-12-12 21:31:25 +0000

    Product model created for the user consisting of make, size, colour, studs, price and image of rugby boots

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    make = models.CharField(max_length=254, default='')
+    """ No default product will be added into the database """
+    size = models.IntegerField()
+    """ Size of the product """
+    colour = models.TextField()
+    """ Product colour """
+    studs = models.TextField()
+    """ Stud configuration of boot. Either moulded or screwin """
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    """ Pricing model will be less than £1m and decimal places = pence """
+    image = models.ImageField(upload_to='images')
+    """ Allow images to be uploaded for our products """
+    
+    def __str__(self):
+        return self.name  # A string will be returned with the name
+        
+        
+        
+    
+
