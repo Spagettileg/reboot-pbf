021d33de9c785c42afc93a57bddcbb644b74af9f
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2019-12-12 20:38:52 +0000

    Cart added as an installed app to ensure success on browser rendering and a context processor added for cart_contents used to populate the context when a template is rendered with a request.

--- reboot/settings.py
+++ reboot/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'home',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
