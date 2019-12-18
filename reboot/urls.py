12dbf39c4f839ff86f1643661257e757d2c35b26
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2019-12-18 12:04:23 +0000

    Imported MEDIA_ROOT from .settings library and url pattern created as images are being used in media, then a specifc media url address will then be required.

--- reboot/urls.py
+++ reboot/urls.py
@@ -22,6 +22,7 @@ from cart import urls as urls_cart
 from products import urls as urls_products
 from products.views import all_products
 from django.views import static
+from .settings import MEDIA_ROOT 
 
 
 urlpatterns = [
@@ -31,4 +32,5 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^cart/', include(urls_cart)),
     url(r'^products/', include(urls_products)),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
