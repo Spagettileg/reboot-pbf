75d4484d24a30400313609ae3ec622f771919fd1
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-10 11:43:05 +0000

    Import urls as urls_categories from categories library and created new url categories pattern on the top table.

--- reboot/urls.py
+++ reboot/urls.py
@@ -20,6 +20,7 @@ from home.views import home
 from home import urls as urls_home
 from cart import urls as urls_cart
 from products import urls as urls_products
+from categories import urls as urls_categories
 from django.views import static
 from .settings import MEDIA_ROOT 
 
@@ -31,5 +32,6 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^cart/', include(urls_cart)),
     url(r'^products/', include(urls_products)),
+    url(r'^categories/', include(urls_categories)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
