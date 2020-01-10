8fa393b7cb886742e9492704c1ae2d13c2a773f7
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-10 23:51:13 +0000

    Comments added to register, login and logout functions.

--- accounts/views.py
+++ accounts/views.py
@@ -1,53 +1,17 @@
 from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
-def index(request):
-    """
-    Return the index html file
-    """
-    return render(request, 'index.html')
-
-@login_required    
-def logout(request):
-    """
-    Log the user out
-    """
-    auth.logout(request)
-    messages.success(request, "You have been successfully logged out")
-    return redirect(reverse('index'))
-    
-def login(request):
-    """
-    Return a Login page
-    """
-    if request.user.is_authenticated:
-        return redirect(reverse('index'))
-    if request.method == "POST":
-        login_form = UserLoginForm(request.POST)
-        
-        if login_form.is_valid():
-            user = auth.authenticate(username=request.POST['username'],
-                                     password=request.POST['password'])
-                                     
-            if user:
-                auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in.")
-                return redirect(reverse('index'))
-            else:
-                login_form.add_error
-                (None, "Your username or password is incorrect.")
-    else:
-        login_form = UserLoginForm()
-    return render(request, 'login.html', {"login_form": login_form})
-    
 def register(request):
     """
     Render the registration page
     """
     if request.user.is_authenticated:
+        messages.success(request, "You are already logged in", 
+                         extra_tags="alert-primary")
         return redirect(reverse('index'))
         
     if request.method == "POST":
@@ -64,8 +28,9 @@ def register(request):
                 messages.success(request, "You have successfully registered")
                 return redirect(reverse('index'))
             else:
-                messages.error(
-                    request, "Unable to register your account at this time")
+                messages.error(request, 
+                               "Unable to register your account at this time!",
+                               extra_tags="alert-primary")
     else:
         # Create an instance of reg form
         registration_form = UserRegistrationForm()  
@@ -75,7 +40,50 @@ def register(request):
     """
     registration_form passed through as a context dictionary.
     """
-    
+
+
+def login(request):
+    """
+    Return a Login page
+    """
+    if request.user.is_authenticated:
+        messages.success(request, "You are already logged in!", 
+                         extra_tags="alert-primary")
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+            password = request.POST['password'])
+            
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!", 
+                                 extra_tags="alert-primary")
+                if request.GET.get('next', False):
+                    return HttpResponseRedirect(request.GET.get('next'))
+                else:
+                    return redirect(reverse('index'))
+            else:
+                login_form.add_error(None,
+                "Your username or password is incorrect!")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {"login_form": login_form})
+
+
+@login_required    
+def logout(request):
+    """
+    Log the user out
+    """
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out!", 
+                     extra_tags="alert-primary")
+    return redirect(reverse('index'))
+
+
 def user_profile(request):
     """
     The users profile page
