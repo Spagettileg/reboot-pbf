9912442f657ffc2d36fb80b0534f928464f0674a
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-10 17:45:30 +0000

    Imported urls as urls_search from search library and new seacrh url pattern created on top table.

--- reboot/urls.py
+++ reboot/urls.py
@@ -21,6 +21,7 @@ from home import urls as urls_home
 from cart import urls as urls_cart
 from products import urls as urls_products
 from categories import urls as urls_categories
+from search import urls as urls_search
 from django.views import static
 from .settings import MEDIA_ROOT 
 
@@ -33,5 +34,6 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^products/', include(urls_products)),
     url(r'^categories/', include(urls_categories)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
