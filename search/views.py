91df5f838aac7540ff29b5c9ee1a7434bfa25216
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-10 17:48:44 +0000

    Run search function defined, with search results being rendered in products.html page.

--- search/views.py
+++ search/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from products.models import Product
 
 # Create your views here.
-def do_search(request):
+def run_search(request):
     products = Product.objects.filter(name__icontains=request.GET['search'])
     return render(request, "products.html", {"products": products})
