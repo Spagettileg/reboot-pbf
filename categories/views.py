3a8c44c83ca8a63666a07c6207b07b0b3036b50e
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-10 12:15:35 +0000

    Category views created to filter rugby boot products.

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
+# Product category views created here.
+# View will consist of view all, junior and adult rugby boot products.
+
+def product_filter(request, category):
+    products = Product.objects.filter(category=category)
+    return render(request, "products.html", {"products": products})
