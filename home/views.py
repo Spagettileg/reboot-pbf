076fef7fc16f88ef6b33832bda2defe9904e4b5b
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-11 00:25:33 +0000

    Function chnaged from home to index to support standard html convention.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
+def index(request):
     """ A view that displays the index page """
-    return render(request, "home.html")
+    return render(request, "index.html")
