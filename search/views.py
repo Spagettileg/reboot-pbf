b427c42043eb7a8779da6473c3ad1ac91d91a567
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 10:53:02 +0000

    Search views filter updated from name to make. This aligns to the prodeucts model design.

--- search/views.py
+++ search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from products.models import Product
 
 # Create your views here.
+
 def search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    products = Product.objects.filter(make__icontains=request.GET['search'])
     return render(request, "products.html", {"products": products})
