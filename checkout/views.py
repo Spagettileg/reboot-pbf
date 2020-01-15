4bd4065221eaf163f66c84ad3c28ca46158db0ec
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 15:37:27 +0000

    Exception created to support user on invlid card details. If statement created to alert user of payment success together with multiple else statements where the user credit card is invalid. Return statement routes user to ‘checkout.html’ page that includes an order form, payment form and link to stripe with a publishable key.

--- checkout/views.py
+++ checkout/views.py
@@ -41,3 +41,21 @@ def checkout(request):
                     description=request.user.email,
                     card=payment_form.cleaned_data['stripe_id']
                 )
+                
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+            
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('home'))
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+    
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
