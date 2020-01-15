1462fa5cdb24097cca451012840b6800bf478273
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 13:26:44 +0000

    Removed numerous field views colour, size, etc and retained boot make only.

--- search/views.py
+++ search/views.py
@@ -4,5 +4,6 @@ from products.models import Product
 # Create your views here.
 
 def search(request):
-    products = Product.objects.filter(make__icontains=request.GET['search'])
+    products = Product.objects.filter(
+        make__icontains=request.GET['search'])
     return render(request, "products.html", {"products": products})
