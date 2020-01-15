1c006cff9342a964bb66f77e209767aee560e7a1
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 14:57:06 +0000

    Import MakePaymentForm and OderForm from .forms library. Forms used to house the checkout process data.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import MakePaymentForm, OrderForm
