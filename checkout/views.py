2e4266e29b44248f0ced9c767cd4923908cea7ee
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 15:26:52 +0000

    Users order confirmed by completing cart entries to cover both quantity and priice to realise a total cost.

--- checkout/views.py
+++ checkout/views.py
@@ -11,3 +11,27 @@ import stripe
 # Create your views here.
 stripe.api_key = settings.STRIPE_SECRET
 
+@login_required()
+def checkout(request):
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                product = get_object_or_404(Product, pk=id)
+                total += quantity * product.price
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=quantity
+                )
+                order_line_item.save()
+            
+            
