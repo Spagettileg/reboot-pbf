6e7824ec5f4b3a4e50a123050570b95c72459c8d
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 19:43:40 +0000

    Imported urls as uels_checkout from checkout library. New checkout url pattern created at the top table to ensure the user can access from anywhere on the website.

--- reboot/urls.py
+++ reboot/urls.py
@@ -21,6 +21,7 @@ from cart import urls as urls_cart
 from products import urls as urls_products
 from categories import urls as urls_categories
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from django.views import static
 from .settings import MEDIA_ROOT 
 
@@ -33,5 +34,6 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^categories/', include(urls_categories)),
     url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
