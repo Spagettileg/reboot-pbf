f2c315a09775aa2ad9c67c4c0dde8c70c583417b
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 14:38:43 +0000

    New class ‘OrderForm’ is created to help the User create their order.

--- checkout/forms.py
+++ checkout/forms.py
@@ -11,3 +11,13 @@ class MakePaymentForm(forms.Form):
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
+    
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = (
+            'full_name', 'phone_number', 'country', 'postcode',
+            'town_or_city', 'street_address1', 'street_address2',
+            'county'
+        )
