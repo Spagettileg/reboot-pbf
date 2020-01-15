2ab597d26f86daf01b6e3a238e3257f4776b7f85
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-15 14:21:32 +0000

    Both Order & OrderLineItem models added to ensure they can be edited via the browser admin panel.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+"""
+Both Order & OrderLineItem models added to ensure they can be edited
+via the admin panel 
+"""
+class OrderLineAdminInline(admin.TabularInline):
+    model = OrderLineItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineAdminInline, )
+
+admin.site.register(Order, OrderAdmin)
