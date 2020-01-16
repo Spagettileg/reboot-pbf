736e420434b8775bb3116244394f1e14f2de313f
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-16 23:13:06 +0000

    If statement updated to redirect user to index page if payment was successful.

--- checkout/views.py
+++ checkout/views.py
@@ -48,7 +48,7 @@ def checkout(request):
             if customer.paid:
                 messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
-                return redirect(reverse('home'))
+                return redirect(reverse('index'))
             else:
                 messages.error(request, "Unable to take payment")
         else:
