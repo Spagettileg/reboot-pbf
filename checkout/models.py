f9f2eecbb8c8afbfbf0310f209a96591e8add3ea
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-17 10:13:54 +0000

    Added 'on-delete' positional argument to ensure user can complete individual line item orders.

--- checkout/models.py
+++ checkout/models.py
@@ -3,10 +3,13 @@ from products.models import Product
 
 # Create your models here.
 class Order(models.Model):
+    """
+    Model to store order customer information. 
+    """
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
+    postcode = models.CharField(max_length=20, blank=True) # Can be left blank
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=False)
@@ -17,10 +20,14 @@ class Order(models.Model):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
         
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False)
-    product = models.ForeignKey(Product, null=False)
-    quantity = models.IntegerField(blank=False)
+    """
+    Model to store individual order items, the Order and Product they relate
+    to and the quantity the customer would like.
+    """
+    order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return "{0} {1} @ {2}".format(
-            self.quantity, self.product.name, self.product.price)
+            self.quantity, self.product.make, self.product.price)
