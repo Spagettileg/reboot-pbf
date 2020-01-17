8b6becc33b8b471ee76b9b0f2fe246c86c2291e8
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-17 17:36:11 +0000

    Static files storage allows the enablement of storage of static files in AWS S3.

--- reboot/settings.py
+++ reboot/settings.py
@@ -158,6 +158,8 @@ AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
