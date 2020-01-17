2472b084f51da479226c3fc98f9a555fc135fa3d
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-17 23:34:00 +0000

    Media storage class created with the purpose of to avoid overwriting existing static files with media files.

--- custom_storages.py
+++ custom_storages.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+"""
+Both Static & Media storage classes created with the purpose of to avoid
+overwriting existing static files with media files.
+"""
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     
