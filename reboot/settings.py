a3f331e91bbcacf61e8f0c7b81b142b0dbd2e8da
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-17 14:52:38 +0000

    If statement created. If default database 'postgresSQL' is not available then fallback to sqlite3 database.

--- reboot/settings.py
+++ reboot/settings.py
@@ -90,8 +90,16 @@ WSGI_APPLICATION = 'reboot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# Production database = dj_database_url (postgresSQL)
+# Development database = sqlite3
 
-DATABASES = {
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    print("Postgres URL not found, using sqlite instead.")
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
