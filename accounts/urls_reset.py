8c562717b855cbeb9d09a905d2b69612c0304ed2
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-17 11:39:56 +0000

    Django package updte to v 1.11.27. Decorators name changes applied.

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
-    url('^$', password_reset,
-        {'post_reset_redirect': reverse_lazy('password_reset_done')},
-        name='password_reset'),
-    url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
-        {'post_reset_redirect': reverse_lazy('password_reset_complete')},
-        name='password_reset_confirm'),
-    url(r'^complete/$', password_reset_complete, name='password_reset_complete'),
+    url('^$', PasswordResetView,
+        {'post_reset_redirect': reverse_lazy('PasswordResetDoneView')},
+        name='PasswordResetView'),
+    url(r'^done/$', PasswordResetDoneView, name='PasswordResetDoneView'),
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView,
+        {'post_reset_redirect': reverse_lazy('PasswordResetCompleteView')},
+        name='PasswordResetConfirmView'),
+    url(r'^complete/$', PasswordResetCompleteView, name='PasswordResetCompleteView'),
 ]
