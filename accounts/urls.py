c483206c457cab5efb8c50f43a767da6fce4f8e1
Ubuntu <ubuntu@ip-172-31-92-107.ec2.internal>
2020-01-17 17:04:46 +0000

    Url pattern for password_reset changed to PasswoedResetView due to updated Django package being installed.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^login/', login, name="login"),
     url(r'^register/', register, name="register"),
     url(r'^profile/', user_profile, name="profile"),
-    url(r'^password-reset/', include(urls_reset))
+    url(r'^PasswordResetView/', include(urls_reset))
 ]
