b14d663af5d7830cc51ed2e392dd89c009c17d60
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-20 21:00:52 +0000

    url pattern name change from user_profile to profile.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, register, user_profile
+from accounts.views import logout, login, register, profile
 from accounts import urls_reset
 
 urlpatterns = [
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
     url(r'^register/', register, name="register"),
-    url(r'^profile/', user_profile, name="profile"),
+    url(r'^profile/', profile, name="profile"),
     url(r'^PasswordResetView/', include(urls_reset))
 ]
