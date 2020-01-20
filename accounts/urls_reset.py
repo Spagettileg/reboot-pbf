70d9d3fdf63fd11d66c0d34a2c652e1bc3ff6b5d
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-20 16:18:02 +0000

    Url patterns updated to include reverse_lazy to reset-done and reset-confirm to clear systme error.

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,15 +1,12 @@
 from django.conf.urls import url
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
-    url(r'^reset-password/$',
-    PasswordResetView.as_view(), name='password_reset'),
-    url(r'^reset-password/done/$',
-    PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-    PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^reset-password/complete/$',
-    PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    url('^$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
+    url(r'^done/$', password_reset_done, name='password_reset_done'),
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
+        {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
+    url(r'^complete/$', password_reset_complete, name='password_reset_complete')
 ]
