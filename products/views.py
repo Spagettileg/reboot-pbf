a3d885714b7d8b0e211f81da7ab659cb609b8706
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-21 13:43:32 +0000

    Single_product_view function added where user is permitted add their comments relating to their Re-boot experience.

--- products/views.py
+++ products/views.py
@@ -1,17 +1,63 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, ProductComment
+from django.utils import timezone
+from django.contrib import messages
+from .forms import ProductCommentForm, ProductCreationForm
+from django.contrib.auth.decorators import login_required
 
 """
 Create product views
 """
+
+""" View to show all products on a single page """
 def all_products(request):
     products = Product.objects.all()
-    return render(request, "products.html", {"products": products})
+    context = {
+        'products': products
+    }
+    return render(request, "products.html", context)
 
+""" View to show Junior & Adult category for rugby boot shopping """
 def categories(request):
     categories = Product.objects.all()
-    return render(request, "products.html", {"categories": categories})
+    context = {
+        'categories': categories
+    }
+    return render(request, "products.html", context)
 
+""" View to show rugby boot product detail, per product """ 
 def product_detail(request, pk):
     products = get_object_or_404(Product, pk=pk)
-    return render(request, "productdetail.html", {"product": products})
+    context = {
+        'product': products
+    }
+    return render(request, "productdetail.html", context)
+
+""" Route to view a single rugby boot product on one page """
+def single_product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    
+    if request.method == 'POST':
+        product_comment_form = ProductCommentForm(request.POST or None)
+        if product_comment_form.is_valid():
+            comment = request.POST.get('comment')
+            product_comment = ProductComment.objects.create(product=product, creator=request.user, comment=comment)
+            product_comment.save()
+            messages.success(request, 'Thank you {} your comment has posted'.format(request.user), extra_tags="alert-success")
+            return redirect(request.META.get('HTTP_REFERER'))
+    else:
+        product_comment_form = ProductCommentForm()
+        product.views += 1
+        product.save()
+    
+    comments = ProductComment.objects.filter(product=product)
+    
+    product_comment_form = ProductCommentForm()
+            
+    context = {
+        'product' : product,
+        'product_comment_form': product_comment_form,
+        'comments': comments,
+    }
+    
+    return render(request, 'single_product.html', context)
