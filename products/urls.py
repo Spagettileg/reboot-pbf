5c6241bacfe5c20994d7289383561d6572179aa7
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-21 13:38:04 +0000

    Import single_product_view, create_a_product and delete_a_product from .views library. New url patterns added for the same decorators.

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail, categories
+from .views import all_products, product_detail, categories, single_product_view, create_a_product, delete_a_product
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^$', categories, name='categories'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product-detail'),
+    url(r'^(?P<pk>\d+)/$', single_product_view, name="single_product_view"),
+    url(r'^(?P<pk>\d+)/delete_feature/$', delete_a_product, name="delete_a_product"),
+    url(r'^create_feature/$', create_a_product, name='create_a_product'),
 ]
