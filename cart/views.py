275ae344ca15f8ab573a495d579c571b01deb38e
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-22 12:52:41 +0000

    adjust-cart function reinstated to enable product quantity tobe adjusted by the user.

--- cart/views.py
+++ cart/views.py
@@ -26,6 +26,22 @@ def add_to_cart(request, id):
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+    
+def adjust_cart(request, id):
+    """
+    Adjust the quantity of the specified product to the specified
+    amount
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
 
 def remove_from_cart(request, id):
     """
