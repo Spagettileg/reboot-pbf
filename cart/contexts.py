f88630ca707bbd1d898005d32e139e5ae71f662d
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-22 13:26:33 +0000

    Updated user comments for improved UXD.

--- cart/contexts.py
+++ cart/contexts.py
@@ -23,6 +23,8 @@ def cart_contents(request):
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
         product_count += quantity
+        print ("quantity: " + quantity)
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
         
     return { 'cart_items': cart_items, 'total': total, 'product_count': product_count}
+    
