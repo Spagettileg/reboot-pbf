21f510d44f70fb059b408bc1ee353dd12b05ba69
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-23 13:24:02 +0000

    Imported urls as urls_feedback from feedback library. New feedback url pattern created at the top table.

--- reboot/urls.py
+++ reboot/urls.py
@@ -22,6 +22,7 @@ from products import urls as urls_products
 from categories import urls as urls_categories
 from search import urls as urls_search
 from checkout import urls as urls_checkout
+from feedback import urls as urls_feedback
 from django.views import static
 from .settings import MEDIA_ROOT 
 
@@ -35,5 +36,6 @@ urlpatterns = [
     url(r'^categories/', include(urls_categories)),
     url(r'^search/', include(urls_search)),
     url(r'^checkout/', include(urls_checkout)),
+    url(r'^feedback/', include(urls_feedback)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
