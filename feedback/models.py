93f126534020438a7387faba2fc9738eaee56121
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-23 14:44:35 +0000

    Model created for customer feedback to include feedback description, detailed comments, creator ID and date feedback created.

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class ProductComment(models.Model):
+    """Re-Boot Member Feedback"""
+    feedbacks = models.ForeignKey(Feedback, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=256, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return '{0}: {1}'.format(self.feedback.title, self.creator.username)
