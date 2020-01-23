019a3003ba00cdbbff8c0125b9829aee27b0138d
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-23 14:48:04 +0000

    ProductComment Class renamed to FeedbackComment and moved to feedback (models) directory.

--- products/models.py
+++ products/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
 
 category_choices = (
     ('Juniors','Juniors'),
@@ -30,17 +28,3 @@ class Product(models.Model):
     
     def __str__(self):
         return self.make  # A string will be returned with the make of rugby boot
-        
-class ProductComment(models.Model):
-    """Rugby Boot Product Comments"""
-    products = models.ForeignKey(Product, on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.TextField(max_length=256, blank=False)
-    created_date = models.DateTimeField(auto_now_add=True)
-    
-    
-    def __str__(self):
-        return '{0}: {1}'.format(self.feature.title, self.creator.username)
-
-    
-
