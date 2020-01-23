077dbf482f9b1a04cf085fae890c58ffc4cf6261
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-23 16:10:37 +0000

    Both feedback and feedback comments made available for viewing in the browser admin panel.

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Feedback, FeedbackComment
 
-# Register your models here.
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'creator', 'created_date']
+    
+class FeedbackCommentAdmin(admin.ModelAdmin):
+    list_display = ['feedback', 'comment', 'creator', 'created_date']
+
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(FeedbackComment, FeedbackCommentAdmin)
