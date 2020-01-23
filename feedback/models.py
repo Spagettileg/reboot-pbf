cf1f949bffbd2620c30eef35d462b3a26668131f
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-23 16:17:05 +0000

    Feedback class added to include field name settings for title, description, creator, created_date and views.

--- feedback/models.py
+++ feedback/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Feedback(models.Model):
+    """ A feedback posting """
+    title = models.CharField(max_length=56, blank=False)
+    description = models.TextField(blank=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    
+    
+    def __str__(self):
+        return self.title
+
 class FeedbackComment(models.Model):
-    """Re-Boot Member Feedback"""
-    feedbacks = models.ForeignKey(Feedback, on_delete=models.CASCADE)
+    """Re-Boot Member Feedback Comments"""
+    feedback = models.ForeignKey(Feedback, on_delete=models.CASCADE)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField(max_length=256, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
