aa3f985850b4af6936ba3bb4b398621b0b036303
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-24 09:45:36 +0000

    New edit a product url pattern created to ensure url links are enabled.

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail, categories, single_product_view, create_a_product, delete_a_product
+from .views import all_products, product_detail, categories, edit_a_product, create_a_product, delete_a_product
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^$', categories, name='categories'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product-detail'),
-    url(r'^(?P<pk>\d+)/$', single_product_view, name="single_product_view"),
+    url(r'^(?P<pk>\d+)/edit_product/$', edit_a_product, name="edit_a_product"),
     url(r'^(?P<pk>\d+)/delete_product/$', delete_a_product, name="delete_a_product"),
     url(r'^create_product/$', create_a_product, name='create_a_product'),
 ]
