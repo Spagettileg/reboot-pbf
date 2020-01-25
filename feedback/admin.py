48b1a9fc801f1a7714b87d3c646d6985f0378391
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-25 13:04:18 +0000

    list filters added. Created data to be viewed at feedback level. Feedback and creator to be viewed at comment level.

--- feedback/admin.py
+++ feedback/admin.py
@@ -3,9 +3,11 @@ from .models import Feedback, FeedbackComment
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'creator', 'created_date']
+    list_filter = ('created_date',)
     
 class FeedbackCommentAdmin(admin.ModelAdmin):
     list_display = ['feedback', 'comment', 'creator', 'created_date']
+    list_filter = ('feedback', 'creator')
 
 admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(FeedbackComment, FeedbackCommentAdmin)
