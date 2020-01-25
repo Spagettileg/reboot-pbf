78b5dda4a6c0be163f29b606184dd8d5ec43424b
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-25 17:00:34 +0000

    Import show all purchases from .views library. New show all purchases url pattern created.

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail, categories, edit_a_product, create_a_product, delete_a_product
+from .views import (all_products, product_detail, categories,
+                    show_all_purchases, edit_a_product, create_a_product,
+                    delete_a_product)
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^$', categories, name='categories'),
+    url(r'^$', show_all_purchases, name="show_all_purchases"),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product-detail'),
     url(r'^(?P<pk>\d+)/edit_product/$', edit_a_product, name="edit_a_product"),
     url(r'^(?P<pk>\d+)/delete_product/$', delete_a_product, name="delete_a_product"),
