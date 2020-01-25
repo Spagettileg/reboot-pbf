6dc9ac6dea8069b3e34aeb20192470c32dd889f0
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-25 17:31:04 +0000

    Import Feedback from feedback.models library. Typo error correction.

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 from products.models import Product
-from feedbacks.models import Feedback 
+from feedback.models import Feedback 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
