c199b48c8a98d50e03b102a029e0e406bc2b39f8
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-27 23:33:29 +0000

    Email files for backend, host, port, use_tls, host_user and host_password added to support contact feature.

--- reboot/settings.py
+++ reboot/settings.py
@@ -197,3 +197,11 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 # Controls where Django stores message data
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+# Email Files -------------------------------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
