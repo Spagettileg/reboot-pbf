3a766c025d6b6fa397203b3d7836907b8869c9c5
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-27 23:52:24 +0000

    Data field narrative changes to name, subject, email address and message. Improved UXD.

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from phonenumber_field.formfields import PhoneNumberField
 
 
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(label='Full name *', widget=forms.TextInput({'placeholder': 'Enter your full name'}), required=True)
-    contact_email = forms.EmailField(label='Email *', widget=forms.TextInput({'placeholder': 'Enter your email'}), required=True)
-    phone_number = PhoneNumberField(error_messages={'invalid': 'Phone number must be 10 or 11 digits.'}, 
-                   widget=forms.TextInput({'placeholder': 'Enter your number'}), required=False)
-    message = forms.CharField(label='Your enquiry *', widget=forms.Textarea({'placeholder': 'Enter your message'}), required=True)
+    name = forms.CharField(required=True)
+    email_address = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(
+        required=True,
+        widget=forms.Textarea()
+    )
+
+    class Meta:
+        fields = [
+            'name', 'subject', 'email_address', 'message'
+        ]
