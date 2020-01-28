57e65d8c1b58c66ebfed00e2d1c304606707faa0
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-01-28 21:31:37 +0000

    Import faqs from .views library. New faqs url pattern created.

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import index, contact, explained
+from .views import index, contact, explained, faqs
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^contact/$', contact, name='contact'),
     url(r'^explained/$', explained, name='explained'),
+    url(r'^faqs/$', faqs, name='faqs'),
 ]
