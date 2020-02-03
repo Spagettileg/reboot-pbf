2b1fd3c31a16c156a295f1c90e41c1f956a9433e
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-03 19:59:12 +0000

    Both edit and delete products removed. Function now restricted to admin panel, as ReBoot retains this level of resposnsibility.

--- products/views.py
+++ products/views.py
@@ -33,16 +33,6 @@ def product_detail(request, id):
     }
     return render(request, "productdetail.html", context)
 
-def show_all_purchases(request, id):
-    products = get_object_or_404(Product, id=id)
-    """
-    Route to show all user purchases on one page
-    """
-    
-    context = {
-        'products': products
-    }
-    return render(request, 'profile.html', context)
 
 """ Route allows the user to create (donate) a product """    
 @login_required
@@ -70,41 +60,3 @@ def create_a_product(request):
     }
     
     return render(request, 'create_product.html', context)
-    
-@login_required
-def edit_a_product(request, id):
-    """
-    Route to permit Re-Boot members to edit their rugby boot product
-    """
-    product = get_object_or_404(Product, pk=id)
-    
-    if request.method == "POST":
-        form = ProductCreationForm(request.POST, instance=product)
-        if form.is_valid():
-            product = form.save()
-            messages.success(request, "Thank you {0}, {1} has been updated."
-                             .format(request.user, product.make),
-                             extra_tags="alert-primary")
-            return redirect(reverse('profile'))
-    
-    else:
-        form = ProductCreationForm(instance=product)
-        messages.error(request, '{} sorry, your product cannot be updated.'.format(request.user), extra_tags="alert-primary")
-        
-    context = {
-        'form': form,
-    }
-    return render(request, 'edit_product.html', context)
-
-""" Route permits user to delete their product(s) """     
-@login_required
-def delete_a_product(request, id):
-    product = get_object_or_404(Product, pk=id)
-    
-    if request.method == "POST":
-        product.delete()
-        messages.success(request, '{} your product has been deleted!'.format(request.user), extra_tags="alert-success")
-        return redirect(request.META.get('HTTP_REFERER'))
-    else:
-        messages.error(request, '{} sorry, your product cannot be deleted.'.format(request.user), extra_tags="alert-primary")
-        return redirect(request.META.get('HTTP_REFERER'))
