23fae9b5477f55223e9fcdd2ca2abc269e04a12f
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-05 11:13:59 +0000

    Post class created to include title, content, created date, published date, user views, tag and image fields in the blog model design.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Single Re-Boot blog post
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+    # 'images' are located in media directory
+    
+    def __unicode__(self):
+        return self.title
