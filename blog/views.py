9c8f2afc64062b041f56f75293b2c5a38e4e2270
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-05 11:54:36 +0000

    get_posts function defined. Route will return list of Posts published prior to now. List of posts will then be rendered to blogposts.html file.

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,15 @@ from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
 
+
+def get_posts(request):
+    """
+    Create a view that will return a list
+    of Posts that were published prior to 'now'
+    and render them to the 'blogposts.html' template
+    """
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
+    
+
