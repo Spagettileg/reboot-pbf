b8ad4bdca7a2d271d2aec88bc5d10e7df8acdcba
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-05 22:02:30 +0000

    author added as a field name.

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
         # Below are editable fields
-        fields = ('title', 'content', 'image', 'tag', 'published_date')
+        fields = ('title', 'author', 'content', 'image', 'tag', 'published_date')
