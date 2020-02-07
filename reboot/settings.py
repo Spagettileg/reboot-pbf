b0938b745d3b7e6187d020a193ec4181de358145
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-07 10:36:04 +0000

    Media files updated to ensure correct rendering of media images in this project.

--- reboot/settings.py
+++ reboot/settings.py
@@ -185,8 +185,8 @@ STATICFILES_DIRS = (
 
 # Media Files -------------------------------------
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 
