a19e0f2ca8c7ba1a020dc7a16aa9d6bc4b85e146
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-08 15:59:26 +0000

    Placeholder messages added to user contact form. Improved UXD.

--- home/forms.py
+++ home/forms.py
@@ -2,13 +2,10 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    email_address = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(
-        required=True,
-        widget=forms.Textarea()
-    )
+    name = forms.CharField(label='Full name *', widget=forms.TextInput({'placeholder': 'Enter your full name'}), required=True)
+    email_address = forms.EmailField(label='Email address *', widget=forms.TextInput({'placeholder': 'Enter your email'}), required=True)
+    subject = forms.CharField(label='Subject *', widget=forms.TextInput({'placeholder': 'Add your subject'}), required=True)
+    message = forms.CharField(label='Your enquiry *', widget=forms.Textarea({'placeholder': 'Enter your message'}), required=True)
 
     class Meta:
         fields = [
