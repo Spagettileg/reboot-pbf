af110f650c9e3078b679b65f5bbac5977990e26e
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-08 16:51:00 +0000

    Updated user comments for improved UXD.

--- search/views.py
+++ search/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from products.models import Product
+from django.contrib import auth, messages
 
 # Create your views here.
 
 def search(request):
+    """
+    Search based on text input - returns products as a list 
+    """
     products = Product.objects.filter(
         make__icontains=request.GET['search'])
     return render(request, "products.html", {"products": products})
