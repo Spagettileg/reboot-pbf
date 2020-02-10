e7f4c912d3c9f8735b86fe2c8d6843e956fd870a
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-10 20:37:21 +0000

    Test created to ensure user can create a blog.

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -1,65 +1,10 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from .forms import UserLoginForm, UserRegistrationForm
+from .forms import BlogPostForm
 
 
-def setUp(self):
-    """Test to create a user"""
-    user = User.objects.create(username='test123')
-    user.set_password('#Qwertyu')
-    user.save()
-
-
-class TestAccountsLoginForm(TestCase):
-    """
-    Ensure login form is operating as expected
-    """
-    def test_login_form_valid_data(self):
-        user = {'username': 'test123', 'password': '#Qwertyu'}
-        form = UserLoginForm(data=user)
-        self.assertTrue(form.is_valid)
-
-
-    def test_login_form_invalid_data(self):
-        form = UserLoginForm(data={'username':'','password':''})
-        self.assertEqual(form.errors, {'username': ['This field is required.'],
-                                       'password': ['This field is required.']})
-
-
-class TestAccountsRegistrationForm(TestCase):
-    """
-    Ensure registration form is operating as expected
-    """
-    def test_successfull_register(self):
-        form = UserRegistrationForm({'username':"test123",
-                                     "password1": "#Qwertyu",
-                                     "password2": "#Qwertyu",
-                                     "email": "test@123test.com"})
+class TestBlogPostForm(TestCase):
+    """Test user can create a Blog"""
+    def test_users_can_create_a_blog(self):
+        form = BlogPostForm({'title', 'author', 'content', 'image',
+                             'tag', 'published_date'})
         self.assertTrue(form.is_valid())
-
-
-    def test_register_with_already_created_username(self):
-        setUp(self)
-        form = UserRegistrationForm({'username':"test123",
-                                     "password1": "#Qwertyu",
-                                     "password2": "#Qwertyu",
-                                     "email": "test@123test.com"})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['username'],
-                                    ['A user with that username already exists.'])
-
-
-    def test_passwords_match(self):
-        form = UserRegistrationForm({'username': 'test123',
-                                     'password1': '#Qwertyu1',
-                                     'password2': '#Qwertyu2'})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors, {'password2': ['Passwords must match!']})
-
-
-    def test_required_fields(self):
-        form = UserRegistrationForm({})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors, {'username': ['This field is required.'],
-                                       'password1': ['This field is required.'],
-                                       'password2': ['This field is required.']})
