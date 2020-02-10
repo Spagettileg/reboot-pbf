193989a2907a168f3cdd00c335a069927129742f
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-10 23:40:07 +0000

    Tests to return a 200 error created for index, adults, bootquality, contact, cookie, explained, faqs, juniors, privacy and terms html pages.

---
+++ home/tests/test_views.py
@@ -0,0 +1,94 @@
+from django.test import TestCase
+from .views import (index, adults, bootquality, contact, cookie, explained,
+                    faqs, juniors, privacy, terms)
+
+
+class test_home_directory_pages(TestCase):
+    """
+    Home page tested to return a positive 200 error code.
+    """
+    def test_home_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'index.html')
+
+
+    """
+    Adults page tested to return a positive 200 error code.
+    """
+    def test_adults_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'adults.html')
+
+
+    """
+    Boot quality page tested to return a positive 200 error code.
+    """
+    def test_boot_quality_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'bootquality.html')
+
+
+    """
+    Contact page tested to return a positive 200 error code.
+    """
+    def test_contact_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'contact.html')
+
+
+    """
+    Cookie page tested to return a positive 200 error code.
+    """
+    def test_cookie_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'cookie.html')
+
+
+    """
+    Re-Boot explained page tested to return a positive 200 error code.
+    """
+    def test_explained_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'explained.html')
+
+
+    """
+    FAQ's page tested to return a positive 200 error code.
+    """
+    def test_faqs_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'faqs.html')
+
+
+    """
+    Juniors page tested to return a positive 200 error code.
+    """
+    def test_juniors_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'juniors.html')
+
+
+    """
+    User privacy page tested to return a positive 200 error code.
+    """
+    def test_privacy_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'privacy.html')
+
+
+    """
+    Terms and conditions page tested to return a positive 200 error code.
+    """
+    def test_terms_page_is_rendered(self):
+        url = self.client.get('/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'terms.html')
