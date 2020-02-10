95fb9746fe0ab502320806d182dc7a99170d0c0a
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-10 21:38:08 +0000

    Coverage report error corrected by creating a test item as a string where object name equals item as a string.

---
+++ blog/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Item
+"""
+All tests performed map over to functions in models.py file
+"""
+
+class TestItemModel(TestCase):
+    def test_item_as_a_string(self):
+        # Test name given to object is equal to the item as a string
+        item = Item(name="Create a Test")
+        self.assertEqual("Create a Test", str(item))
