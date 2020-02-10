d842ccb610ab495a7c3999b5a43b2cd06fa1d48c
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-10 23:12:53 +0000

    Tests added to support user login and correct rendering of checkout page.

---
+++ checkout/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+
+class test_checkout_views(TestCase):
+    """test for login"""
+    def setUp(self):
+        self.user = User.objects.create_user(username="test123", password="#Qwertyu")
+        self.client.login(username="test123", password="#Qwertyu")
+
+    """test rendering of checkout page via 200 error code generation."""    
+    def test_checkout_page(self):
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout.html')
