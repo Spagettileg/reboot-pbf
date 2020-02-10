720160b887cef2106e55998578d0dc15e0482e7e
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-10 21:18:16 +0000

    Test built for 200 status code on get posts, post detail, new post and edit post functions.

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,68 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib import auth
 from django.contrib.auth.models import User
-from django.test import Client
+from .models import Blog
+from django.shortcuts import get_object_or_404
 
 
-
-class test_views(TestCase):
+class test_blog_views(TestCase):
     """
-    accounts app views tests schedule for login, logout, register & profile.
+    blog app views tests schedule to get blog post, post  blog detail and
+    create or edit a blog post.
     """
     
-    def setUp(self):
-        user = User.objects.create(username='test123')
-        user.set_password('#Qwertyu')
-        user.save()
-        self.client = Client()
-
-
-    def test_login_page(self):
-        url = self.client.get('/accounts/login/')
+    def test_get_posts(self):
+        url = self.client.get('/blog/get_posts/')
         self.assertEqual(url.status_code, 200)
-        self.assertTemplateUsed(url, "login.html")
+        self.assertTemplateUsed(url, "blogposts.html")
         
     
-    def test_login_post(self):
-        log_in = {'username':'test123', 'password' : '#Qwertyu'}
-        response = self.client.post('/accounts/login/', log_in)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/', status_code=302)
+    def test_post_detail(self):
+        response = self.client.get('/blog/post_detail/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'postdetail.html')
+    
+    
+    def test_new_post(self):
+        response = self.client.get('/blog/new_post/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blogpostform.html')
         
     
-    def test_user_logged_in(self):
-        c = Client()
-        c.login(username='test123', password='#Qwertyu')
-        client_user = auth.get_user(c)
-        user = User.objects.get(username='test123')
-        self.assertEqual(client_user, user)
-
-
-    def test_register_page(self):
-        url = self.client.get('/accounts/register/')
-        self.assertEqual(url.status_code, 200)
-        self.assertTemplateUsed(url, "register.html")
-        
-    
-    def test_adding_an_account(self):
-        sign_up = {'email': 'test@123test.com',
-                                    'username': 'test123',
-                                    'password1':'#Qwertyu',
-                                    'password2':'#Qwertyu'}
-        response = self.client.post('/accounts/register/', sign_up)
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/', status_code=302)
-        
-    
-    def test_user_profile_page(self):
-        url = self.client.get('/accounts/profile/')
-        self.assertEqual(url.status_code, 302)
-        self.assertRedirects(url, '/accounts/login/?next=/accounts/profile/',
-                             status_code=302)
-        
-        
-    def test_logging_out(self):
-        url = self.client.get('/accounts/logout/')
-        self.assertEqual(url.status_code, 302)
-        self.assertRedirects(url, '/accounts/login/?next=/accounts/logout/',
-                             status_code=302)
+    def test_edit_post(self):
+        response = self.client.get('/blog/edit_post/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blogpostform.html')
