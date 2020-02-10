3bedc30c6c071d8a5ea49320798de7c80028d5a4
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-10 20:18:53 +0000

    Test to make sure blog directory exists within the Re-Boot app.

--- blog/tests/test_app.py
+++ blog/tests/test_app.py
@@ -1,9 +1,9 @@
 from django.apps import apps
 from django.test import TestCase
-from accounts.apps import AccountsConfig
+from blog.apps import BlogConfig
 
-# Test to ensure accounts directory exists within Re-Boot app 
-class AccountsConfigTest(TestCase):
+# Test to ensure blog directory exists within Re-Boot app 
+class BlogsConfigTest(TestCase):
     def test_apps(self):
-        self.assertEqual(AccountsConfig.name, 'accounts')
-        self.assertEqual(apps.get_app_config('accounts').name, 'accounts')
+        self.assertEqual(BlogConfig.name, 'blog')
+        self.assertEqual(apps.get_app_config('blog').name, 'blog')
