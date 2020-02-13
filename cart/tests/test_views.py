cdf0b2fc6bff5595dc172b6d08c1be6624086ea6
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-13 15:04:16 +0000

    White space removed to ensure PEP8 compliance.

--- cart/tests/test_views.py
+++ cart/tests/test_views.py
@@ -4,14 +4,15 @@ from django.contrib.auth.models import User
 from django.test import Client
 from products.models import Product
 
+
 class Test_Cart_Views(TestCase):
     """Series of tests to support cart directory views"""
-    
+
     def setUp(self):
         """Logs in a user for our tests"""
-        user = User.objects.create_user(username="test123", password="#Qwertyu")
+        self.user = User.objects.create_user(username="test123",
+                                             password="#Qwertyu")
         self.client.login(username="test123", password="#Qwertyu")
-
 
     def test_status_codes(self):
         """Test for app to return error status code 200"""
@@ -19,7 +20,6 @@ class Test_Cart_Views(TestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, 'cart.html')
 
-    
     def test_add_to_cart(self):
         """Test for items to be added to cart"""
         user = User(username="test123")
@@ -27,6 +27,6 @@ class Test_Cart_Views(TestCase):
 
         product = Product(title="test", description="testing", creator=user)
         product.save()
-        
+
         page = self.client.get('/cart/add/{0}'.format(product.id))
         self.assertEqual(page.status_code, 301)
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
+
 def view_cart(request):
     """
     A view that renders the shopping cart showing all contents page
     """
     return render(request, "cart.html")
+
 
 def add_to_cart(request, id):
     """
@@ -20,21 +22,21 @@ def add_to_cart(request, id):
     # a duplicate product.
 
     if id in cart:
-        cart[id] = int(cart[id]) + quantity 
+        cart[id] = int(cart[id]) + quantity
     else:
         cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
+
 def adjust_cart(request, id):
     """
     Adjust the quantity of the specified product to the specified
     amount
     """
-    
     quantity = int(request.POST.get('quantity'))
-    
+
     cart = request.session.get('cart', {})
 
     if quantity > 0:
@@ -44,6 +46,7 @@ def adjust_cart(request, id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
 
 def remove_from_cart(request, id):
     """
