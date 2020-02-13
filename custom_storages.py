88dcd9f253f5d3ed891dce6659a52cfd7c493299
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-13 22:55:07 +0000

    White space removed to enable PEP8 compliance.

--- custom_storages.py
+++ custom_storages.py
@@ -5,8 +5,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 Both Static & Media storage classes created with the purpose of to avoid
 overwriting existing static files with media files.
 """
+
+
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    
+
+
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
