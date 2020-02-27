9aea3925b04963477ac00d59c93bfa4cf1c954dc
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-02-27 15:01:55 +0000

    Updated user comments for improved UXD.

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,5 @@ $(document).ready(function () {
     // Current year for the copyright date appears in the footer
      
     $('#trading-year').text('2019 - ' + new Date().getFullYear());
+    
 });
