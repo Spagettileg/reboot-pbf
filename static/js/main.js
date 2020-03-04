373651d028c0ab220963506fddc75b856a9062a9
Ubuntu <ubuntu@ip-172-31-80-103.ec2.internal>
2020-03-04 00:36:44 +0000

    Cart pop over function created and powered by jQuery.

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
     // Re-Boot trading years appear in the footer
      
     $('#trading-year').text('2019 - ' + new Date().getFullYear());
+    
+    // Shopping cart popover
+    
+    $('#cart-popover').popover({
+        html : true,
+        container: 'body',
+        content: function() {
+            return $('#popover_content_wrapper').html();
+        }
+    });
 });
