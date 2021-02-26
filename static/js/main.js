93c6b42e2c862dfd6966a467209660a1859b19cb
Ubuntu <ubuntu@ip-172-31-35-90.eu-west-1.compute.internal>
2021-02-26 15:49:18 +0000

    New back to top function added. Button appears onec euser has scrolled minimum of 575px.

--- static/js/main.js
+++ static/js/main.js
@@ -14,3 +14,21 @@ $(document).ready(function () {
         }
     });
 });
+
+// Scroll to top of webpage...
+
+    $("a[href='#move-up']").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+    // If position of vertical scroll is above 575px, for 
+    // return to top button to disappear.
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 575) {
+            $('.move-top').fadeIn();
+        } else {
+            $('.move-top').fadeOut();
+        }
+    });
