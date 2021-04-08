cfb1fd92cfc34e61335f6200d69adcdb896d7fbc
Ubuntu <ubuntu@ip-172-31-35-90.eu-west-1.compute.internal>
2021-04-08 16:22:23 +0000

    Search views replicate the form design to ensure user can optimise their search performance.

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from products.models import Product
 from django.contrib import auth, messages
+from django.db.models import Q
 
 # Create your views here.
 
@@ -8,13 +9,47 @@ def search(request):
     """
     Search based on text input - returns products as a list 
     """
-    products = Product.objects.filter(
-        make__icontains=request.GET['search'],
-        category__icontains=request.GET['search'],
-        customer__icontains=request.GET['search'],
-        size__icontains=request.GET['search'],
-        colour__icontains=request.GET['search'],
-        studs__icontains=request.GET['search'],
-        quality__icontains=request.GET['search'],
-        price__icontains=request.GET['search'])
-    return render(request, "products.html", {"products": products})
+    products = None
+    query = None
+    product_count = 0
+    
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.warning(request, 'Please enter a search criteria')
+            return redirect(reverse('products'))
+        products = Product.objects.all().filter(
+            Q(make__icontains=query)
+            | Q(category__icontains=query)
+            | Q(customer__icontains=query)
+            | Q(size__icontains=query)
+            | Q(colour__icontains=query)
+            | Q(studs__icontains=query)
+            | Q(quality__icontains=query)
+            | Q(price__icontains=query)
+            )
+        product_count = Product.objects.all().filter(
+            Q(make__icontains=query)
+            | Q(category__icontains=query)
+            | Q(customer__icontains=query)
+            | Q(size__icontains=query)
+            | Q(colour__icontains=query)
+            | Q(studs__icontains=query)
+            | Q(quality__icontains=query)
+            | Q(price__icontains=query)
+            ).count()
+
+        if product_count == 0:
+            messages.warning(request, ('No results found'
+                                       + f' for the search: {query}.'))
+            return redirect(reverse('products'))
+
+        context = {
+            'query': query,
+            'products': products,
+            'count': product_count,
+        }
+
+        return render(request, 'search/search.html', context)
+
+    return redirect(reverse('products'))
