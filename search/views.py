32cfcde464fc0534fea59622a4b9c6c578e4e8b7
Ubuntu <ubuntu@ip-172-31-35-90.eu-west-1.compute.internal>
2021-04-08 14:38:21 +0000

    Additional search criteria adddd to function.

--- search/views.py
+++ search/views.py
@@ -9,5 +9,12 @@ def search(request):
     Search based on text input - returns products as a list 
     """
     products = Product.objects.filter(
-        make__icontains=request.GET['search'])
+        make__icontains=request.GET['search'],
+        category__icontains=request.GET['search'],
+        customer__icontains=request.GET['search'],
+        size__icontains=request.GET['search'],
+        colour__icontains=request.GET['search'],
+        studs__icontains=request.GET['search'],
+        quality__icontains=request.GET['search'],
+        price__icontains=request.GET['search'])
     return render(request, "products.html", {"products": products})
